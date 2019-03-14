521421030683d260a73e755fc4d6e19b89ebe2c3
hvzzzz <42120805+hvzzzz@users.noreply.github.com>
2019-03-14 01:35:28 -0500

    prov

--- experiment.py
+++ experiment.py
@@ -3,7 +3,7 @@ from numpy import random as rn
 
 for i in range(5):
     #print(i)
-    arr = np.arange(i)  # [0, 1, 2, 3, 4]
+    #arr = np.arange(i)  # [0, 1, 2, 3, 4]
     #random.seed(1)  # Reset random state
     #rn.shuffle(arr)  # Shuffle!
     print (arr)
